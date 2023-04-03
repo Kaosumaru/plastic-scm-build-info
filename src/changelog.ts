b8bba6c9d4cade93a65d3d75886d67fd622f2b51
Mateusz Borycki <mateuszborycki@gmail.com>
2023-04-03 21:48:18 +0200

    Working changelog.

--- src/changelog.ts
+++ src/changelog.ts
@@ -39,6 +39,35 @@ async function mergeToChangelogEntry(
   }
 }
 
+async function checkinToChangelogEntry(
+  options: Options,
+  checkin: string
+): Promise<ChangelogEntry> {
+  const jiraRegex = /(RAISE-\d+)/g
+  const matches = jiraRegex.exec(checkin)
+  let description = checkin
+
+  if (!matches || matches.length < 2) {
+    return {
+      description
+    }
+  }
+
+  const issueNumber = matches[1]
+  const url = `https://${options.host}/browse/${issueNumber}`
+
+  try {
+    const issue = await options.jira.findIssue(issueNumber)
+    description = `${issueNumber} ${issue.fields.summary} - ${checkin}`
+    // eslint-disable-next-line no-empty
+  } catch {}
+
+  return {
+    url,
+    description
+  }
+}
+
 async function mergesToJira(options: Options): Promise<ChangelogEntry[]> {
   const awaitableResult = options.info.merges
     .filter(entry => entry.includes(options.jiraPrefix))
@@ -47,18 +76,33 @@ async function mergesToJira(options: Options): Promise<ChangelogEntry[]> {
   return await Promise.all(awaitableResult)
 }
 
+function shouldIncludeCheckin(options: Options, checkin: string): boolean {
+  if (checkin.includes(options.jiraPrefix)) return true
+  if (options.checkinTags.some(tag => checkin.includes(tag))) return true
+  return false
+}
+
+async function commitsToJira(options: Options): Promise<ChangelogEntry[]> {
+  const awaitableResult = options.info.checkins
+    .filter(entry => shouldIncludeCheckin(options, entry))
+    .map(async entry => checkinToChangelogEntry(options, entry))
+
+  return await Promise.all(awaitableResult)
+}
+
 export interface Options {
   host: string
   jira: JiraApi
-  commitTag: string
   jiraPrefix: string
+  checkinTags: string[]
   info: InfoData
 }
 
 export async function changesToChangelog(options: Options): Promise<Changelog> {
-  const entries: ChangelogEntry[] = []
+  let entries: ChangelogEntry[] = []
 
-  entries.concat(await mergesToJira(options))
+  entries = entries.concat(await mergesToJira(options))
+  entries = entries.concat(await commitsToJira(options))
 
   return {
     entries
